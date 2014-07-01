8decf0215b138613d38a79c4280394d79d0dfe0d
bzq <bzq@zongler.ornl.gov>
2014-07-01 16:15:50 -0400

    Got a (semi) working version of the projection.  Trouble lies as the projection approaches 1, since Pn(1)=1 for all n.

--- projection.py
+++ projection.py
@@ -1,6 +1,6 @@
+#!/bin/python
 '''
 A quick mockup for projecting Nin points onto Nout points in the GLL basis
-
 ''' 
 import time
 import numpy as np
@@ -9,32 +9,50 @@ import matplotlib.pyplot as plot
 from scipy.interpolate import interp1d
 
 Nin = 10                                    # Number of data points in 
-Nout = 20                                   # Number of points we want to project onto
+Nout = 30                                    # Number of points we want to project onto
 Nfine = Nin**2                              # Number of points for plotting pretty 
 Xin = np.linspace(-1,1,Nin)                 # The space of points
-Fin = np.cos(Xin)                           # Evaluate some fake data
+Fin = np.cos(20*Xin)+np.sin(7*Xin)          # Evaluate some fake data
 Xfine = np.linspace(-1,1,Nfine)             # Linspace for plotting pretty
 Fspline = interp1d(Xin,Fin,kind='cubic')    # A spline interpolation of the data
 Xout = np.linspace(-1,1,Nout-1)             # Probably don't need this since we want to plot at the GLL nodes
 I = np.eye(Nout)                            # A hack for choosing the right polynomial orders 
-
+zero = np.zeros(Nout-1)
 
 # Build our set of GLL nodes
-indices = np.zeros([Nout,1])
-indices[-1] = 1                 # Corresponds to using the Nout'th Legendre polynomial
-indices = np.hstack(indices)  
-Xi = legendre.legroots(indices) # Something about these doesn't seem quite right - double check me
+pts = legendre.legroots(legendre.legder(I[-1])) # GLL points
+Xi = [-1, list(pts), 1]
+Xi = np.hstack(Xi)
+print "GLL Points:", Xi
+
+# Build the weight set
+weights = np.zeros([len(Xi)])
+for i in range(len(Xi)-2):
+    weights[i+1] = 2/((Nout)*(Nout-1)*(legendre.legval(Xi[i+1],I[-1]))**2)
+
+weights[0]=2.0/((Nout)*(Nout-1))
+weights[-1]=weights[0]
+print "GLL Weights:", weights
 
 
-# So it would seem we do need the weights for something ( this is a terrible approximation ).
-b = np.zeros([len(Xi)])
+''' 
+# Construct b using nearest known data points
+b = np.zeros(len(Xi))
+for i in range(len(Xi)):          # For each data point
+    for j in range(len(Xi)):      # Need to sum over all basis functions
+            b[i]+=legendre.legval(Xi[i],I[j])*Fin[ np.abs(Xin-Xi[i]).argmin() ] 
+''' 
+# Construct b using a spline interpolation evaluated at the quadrature points
+b = np.zeros(len(Xi))
 for i in range(len(Xi)):
-    print legendre.legval(Xi[i], I[i])
-    b[i] = Fspline(Xi[i])*legendre.legval(Xi[i],I[i])
-
+    for j in range(len(Xi)):
+        b[i]+=legendre.legval(Xi[i],I[j])*Fspline(Xi[i])
+        
+print b
+#'''    
 # Plot what we got
-plot.plot(Xin,Fin,'o', Xfine,Fspline(Xfine) , Xi, b,'x--')
-plot.legend(['data', 'cubic', 'L2 Projection'], loc='best')
+plot.plot(Xin,Fin,'o-' , Xi, b,'rx-')
+plot.legend(['Data', 'L2 Projection'], loc='best')
 plot.draw()
 plot.show()
 
