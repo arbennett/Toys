1408e12f27670c877ae6cb2ab3bee23136e3a10d
arbennett <bennett.andr@gmail.com>
2014-06-30 21:49:39 -0400

    Added some documentation, and found some places for improvement.

--- projection.py
+++ projection.py
@@ -1,5 +1,5 @@
 '''
-A quick mockup for projecting n points onto m points in the GLL basis
+A quick mockup for projecting Nin points onto Nout points in the GLL basis
 
 ''' 
 import time
@@ -8,29 +8,32 @@ import numpy.polynomial.legendre as legendre
 import matplotlib.pyplot as plot
 from scipy.interpolate import interp1d
 
-Nin = 10
-Nout = 20
-Nfine = Nin**2
-Xin = np.linspace(-1,1,Nin)
-Fin = np.cos(Xin)
-Xfine = np.linspace(-1,1,Nfine)
-Fspline = interp1d(Xin,Fin,kind='cubic')
-Xout = np.linspace(-1,1,Nout-1)
-I = np.eye(Nout)
+Nin = 10                                    # Number of data points in 
+Nout = 20                                   # Number of points we want to project onto
+Nfine = Nin**2                              # Number of points for plotting pretty 
+Xin = np.linspace(-1,1,Nin)                 # The space of points
+Fin = np.cos(Xin)                           # Evaluate some fake data
+Xfine = np.linspace(-1,1,Nfine)             # Linspace for plotting pretty
+Fspline = interp1d(Xin,Fin,kind='cubic')    # A spline interpolation of the data
+Xout = np.linspace(-1,1,Nout-1)             # Probably don't need this since we want to plot at the GLL nodes
+I = np.eye(Nout)                            # A hack for choosing the right polynomial orders 
+
 
 # Build our set of GLL nodes
 indices = np.zeros([Nout,1])
-indices[-1] = 1
-indices = np.hstack(indices)
-Xi = legendre.legroots(indices) # Still need to find out how to add +-1 at endpoints 
+indices[-1] = 1                 # Corresponds to using the Nout'th Legendre polynomial
+indices = np.hstack(indices)  
+Xi = legendre.legroots(indices) # Something about these doesn't seem quite right - double check me
 
+
+# So it would seem we do need the weights for something ( this is a terrible approximation ).
 b = np.zeros([len(Xi)])
 for i in range(len(Xi)):
     print legendre.legval(Xi[i], I[i])
     b[i] = Fspline(Xi[i])*legendre.legval(Xi[i],I[i])
 
-print len(Xout), len(Xi)
-plot.plot(Xin,Fin,'o', Xfine,Fspline(Xfine) , Xout, b,'x--')
+# Plot what we got
+plot.plot(Xin,Fin,'o', Xfine,Fspline(Xfine) , Xi, b,'x--')
 plot.legend(['data', 'cubic', 'L2 Projection'], loc='best')
 plot.draw()
 plot.show()
