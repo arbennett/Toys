81a27f229412c9bc342238e24d6ce998ba57998e
arbennett <bennett.andr@gmail.com>
2017-08-18 01:04:25 -0700

    Fix format

--- maths/n_body_benchmark/julia/native.jl
+++ maths/n_body_benchmark/julia/native.jl
@@ -6,20 +6,20 @@ const dt = parse(Float64, ARGS[3])
 
 function nbody(size::Int64, nstep::Int64, dt::Float64)
     G :: Float64 = 6.6741e-11
-	pos :: Array{Float64, 2} = rand(Float64, (size, 2))
-	vel :: Array{Float64, 2} = rand(Float64, (size, 2))
-	dv_abs :: Float64 = 0.0
+    pos :: Array{Float64, 2} = rand(Float64, (size, 2))
+    vel :: Array{Float64, 2} = rand(Float64, (size, 2))
+    dv_abs :: Float64 = 0.0
     # Update velocity
     for i=1:size, j=1:size
         if i != j
             dv_abs = G / (sqrt((pos[i, 1] - pos[j, 1])^2 + (pos[i, 2] - pos[j, 2])^2)^3)
-			vel[j] += dv_abs * dt * (pos[j] - pos[i])
-		end
-	end
+            vel[j] += dv_abs * dt * (pos[j] - pos[i])
+	    end
+    end
     # Update position
     for i = 1:size
         pos[i] += dt * vel[i]
-	end
+    end
 end
 
 
